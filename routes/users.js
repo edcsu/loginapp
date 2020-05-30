596d48fa666b88d7670559d5526194f980537597
Keith Ssewanonda <keith@laboremus.no>
2020-05-30 16:17:50 +0300

    feat: added logout route

--- routes/users.js
+++ routes/users.js
@@ -103,4 +103,14 @@ router.post('/login',
         res.redirect('/');
     });
 
+// logout
+router.get('/logout',
+    (req, res) => {
+        req.logOut();
+
+        req.flash('success_msg', 'You are logged out.')
+
+        res.redirect('/users/login');
+    });
+
 module.exports = router;
