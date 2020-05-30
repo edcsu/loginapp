5ae5cec0c331f119da5a5f6ce2f1fb1c529cb868
Keith Ssewanonda <keith@laboremus.no>
2020-05-30 14:57:30 +0300

    feat: add login logic

--- models/user.js
+++ models/user.js
@@ -22,9 +22,27 @@ const UserSchema = mongoose.Schema({
 let User = module.exports = mongoose.model('User', UserSchema);
 
 module.exports.createUser = function (newUser, callback) {
-    bcrypt.hash(newUser.password, saltRounds, function (err, hash) {
+    bcrypt.hash(newUser.password, saltRounds, (err, hash) => {
         // store hash in the db
         newUser.password = hash;
         newUser.save(callback);
+    });
+}
+
+module.exports.getUserByUsername = (username, callback) => {
+    let query = { username };
+    User.findOne(query, callback);
+}
+
+module.exports.getUserById = (id, callback) => {
+    User.findById(id, callback);
+}
+
+module.exports.comparePassword = (candidatePassword, hash, callback) => {
+    bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+        if (err) {
+            throw err;
+        }
+        callback(null, isMatch)
     });
 }
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
 const router = express.Router();
 const { check, body, validationResult } = require('express-validator');
 const User =  require('../models/user')
@@ -55,5 +57,50 @@ router.post('/register',[
 router.get('/login', (req, res) => {
     res.render('login');
 });
+
+// local strategy
+passport.use( new localStrategy(
+    (username, password, done) => {
+        User.getUserByUsername(username, (err, user) =>{
+            if (err) {
+                throw err;
+            }
+            if (!user) {
+                return done(null, false, { message: 'Unknown User'});
+            }
+
+            User.comparePassword(password, user.password, (err, isMatch) => {
+                if (err) {
+                    throw err;
+                }
+                if (isMatch) {
+                    return done(null, false);
+                } else {
+                    return done(null, false, { message: 'Invalid password'});
+                }
+            });
+        });
+}));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+  
+passport.deserializeUser(function(id, done) {
+    User.getUserById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+// login a user
+router.post('/login',
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/users/login',
+        failureFlash: true
+    }),
+    (req, res) => {
+        res.redirect('/');
+    });
 
 module.exports = router;
